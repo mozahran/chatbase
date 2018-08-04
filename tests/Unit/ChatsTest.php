4953a51651727af85f240cf6dd98d27ab82d9c5b
Mohamed Zahran <m.zahran90@gmail.com>
2018-08-04 14:28:32 +0200

    Refactor the test cases and use dummy factory methods

--- tests/Unit/ChatsTest.php
+++ tests/Unit/ChatsTest.php
@@ -4,10 +4,12 @@ namespace Tests\Unit;
 
 use App\Conversation;
 use App\ConversationReply;
+use App\ConversationReplyUser;
+use App\ConversationUser;
+use App\Managers\ChatManager;
+use App\Managers\Contracts\ChatManagerInterface;
 use App\Repositories\ChatRepository;
 use App\Repositories\Interfaces\ChatRepositoryInterface;
-use App\Repositories\Interfaces\UserRepositoryInterface;
-use App\Repositories\UserRepository;
 use App\User;
 use Carbon\Carbon;
 use Tests\TestCase;
@@ -17,134 +19,132 @@ class ChatsTest extends TestCase
 {
     use RefreshDatabase;
 
-    /** @var ChatRepositoryInterface */
+    /**
+     * @var ChatRepositoryInterface
+     */
     private $repository;
+
+    /**
+     * @var ChatManagerInterface
+     */
+    private $manager;
 
     public function __construct(string $name = null, array $data = [], string $dataName = '')
     {
         parent::__construct($name, $data, $dataName);
 
         $this->repository = app(ChatRepository::class);
+        $this->manager = app(ChatManager::class);
     }
 
     public function testCreateConversation()
     {
-        $creator = factory(User::class)->create();
-        $recipient = factory(User::class)->create();
+        $creator = $this->createUser();
+        $recipient = $this->createUser();
 
-        $created = $this->repository->createConversation($creator, [$creator, $recipient]);
+        $created = $this->manager->createConversation($creator, [$creator, $recipient]);
 
         $this->assertNotNull($created);
     }
 
     public function testAddUserToConversation()
     {
-        $user = factory(User::class)->create();
+        $user = $this->createUser();
+        $conversation = $this->createConversation($user);
 
-        $conversation = factory(Conversation::class)->create([
-            Conversation::FIELD_CREATOR_ID => $user->getId(),
-        ]);
-
-        $added = $this->repository->addUserToConversation($user, $conversation);
+        $added = $this->manager->addUserToConversation($user, $conversation);
 
         $this->assertNotNull($added);
     }
 
     public function testGetConversations()
     {
-        $firstUser = factory(User::class)->create();
-        $secondUser = factory(User::class)->create();
-        $thirdUser = factory(User::class)->create();
+        $user = $this->createUser();
+        $conversation = $this->createConversation($user);
 
-        $this->repository->createConversation($firstUser, [$firstUser, $secondUser]);
-        $this->repository->createConversation($secondUser, [$secondUser, $thirdUser]);
-
-        $results = $this->repository->getConversations($firstUser);
+        $results = $this->repository->getConversations($user);
 
         $this->assertCount(1, $results);
     }
 
     public function testGetConversation()
     {
-        $creator = factory(User::class)->create();
-        $recipient = factory(User::class)->create();
+        $user = $this->createUser();
+        $conversation = $this->createConversation($user);
 
-        $c = $this->repository->createConversation($creator, [$creator, $recipient]);
-
-        $found = $this->repository->getConversation($c->getId(), $creator);
+        $found = $this->repository->getConversation($conversation->getId(), $user);
 
         $this->assertNotNull($found);
     }
 
     public function testDeleteConversation()
     {
-        $creator = factory(User::class)->create();
-        $recipient = factory(User::class)->create();
+        $user = $this->createUser();
+        $conversation = $this->createConversation($user);
 
-        $c = $this->repository->createConversation($creator, [$creator, $recipient]);
-
-        $deleted = $this->repository->deleteConversation($c->getId(), $creator);
+        $deleted = $this->manager->deleteConversation($conversation, $user);
 
         $this->assertTrue($deleted);
     }
 
+    public function testCountConversationUsers()
+    {
+        $user = $this->createUser();
+        $conversation = $this->createConversation($user);
+
+        $count = $this->repository->countConversationUsers($conversation);
+
+        $this->assertEquals(1, $count);
+    }
+
     public function testCreateReply()
     {
-        $sender = factory(User::class)->create();
+        $user = $this->createUser();
+        $conversation = $this->createConversation($user);
 
-        $conversation = factory(Conversation::class)->create([
-            Conversation::FIELD_CREATOR_ID => $sender->getId(),
-        ]);
-
-        $created = $this->repository->createReply($conversation, $sender, 'foo');
+        $created = $this->manager->createReply($conversation, $user, 'foo');
 
         $this->assertNotNull($created);
     }
 
     public function testGetReplies()
     {
-        $creator = factory(User::class)->create();
-        $recipient = factory(User::class)->create();
+        $user = $this->createUser();
+        $conversation = $this->createConversation($user);
 
-        $conversation = $this->repository->createConversation($creator, [$creator, $recipient]);
+        $this->createReply($conversation, $user, 'foo');
+        $this->createReply($conversation, $user, 'bar');
 
-        $this->repository->createReply($conversation, $creator, 'foo');
-        $this->repository->createReply($conversation, $creator, 'bar');
-
-        $results = $this->repository->getReplies($conversation, $creator);
+        $results = $this->repository->getReplies($conversation, $user);
 
         $this->assertCount(2, $results);
     }
 
     public function testGetNewReplies()
     {
-        $firstUser = factory(User::class)->create();
-        $secondUser = factory(User::class)->create();
-
-        $conversation = $this->repository->createConversation($firstUser, [$firstUser, $secondUser]);
+        $user = $this->createUser();
+        $conversation = $this->createConversation($user);
 
         $yesterday = Carbon::now()->subDay();
         $sinceAnHour = Carbon::now()->subHour();
 
-        $this->repository->createReply($conversation, $secondUser, 'foo', $yesterday);
-        $this->repository->createReply($conversation, $secondUser, 'bar', $sinceAnHour);
+        $this->createReply($conversation, $user, 'foo', $yesterday);
+        $this->createReply($conversation, $user, 'bar', $sinceAnHour);
 
-        $results = $this->repository->getNewReplies($conversation, $firstUser, $yesterday);
+        $results = $this->repository->getNewReplies($conversation, $user, $yesterday);
 
         $this->assertCount(1, $results);
     }
 
     public function testGetConversationWithReplies()
     {
-        $firstUser = factory(User::class)->create();
-        $secondUser = factory(User::class)->create();
+        $user = $this->createUser();
+        $conversation = $this->createConversation($user);
 
-        $conversation = $this->repository->createConversation($firstUser, [$firstUser, $secondUser]);
+        $this->createReply($conversation, $user, 'foo');
+        $this->createReply($conversation, $user, 'bar');
 
-        $this->repository->createReply($conversation, $firstUser, 'foo');
-        $this->repository->createReply($conversation, $secondUser, 'bar');
-
-        $results = $this->repository->getConversationWithReplies($conversation, $firstUser);
+        $results = $this->repository->getConversationWithReplies($conversation, $user);
 
         $this->assertNotNull($results);
         $this->assertNotNull($results->replies);
@@ -152,15 +152,52 @@ class ChatsTest extends TestCase
 
     public function testDeleteReply()
     {
-        $firstUser = factory(User::class)->create();
-        $secondUser = factory(User::class)->create();
+        $user = $this->createUser();
+        $conversation = $this->createConversation($user);
+        $reply = $this->createReply($conversation, $user, 'foo');
 
-        $conversation = $this->repository->createConversation($firstUser, [$firstUser, $secondUser]);
-
-        $reply = $this->repository->createReply($conversation, $firstUser, 'foo');
-
-        $deleted = $this->repository->deleteReply($reply, $firstUser);
+        $deleted = $this->manager->deleteReply($reply, $user);
 
         $this->assertTrue($deleted);
+    }
+
+    // ----------------------------------------------------------------------
+    // Dummy Factory Methods
+    // ----------------------------------------------------------------------
+
+    private function createUser()
+    {
+        return factory(User::class)->create();
+    }
+
+    private function createConversation(User $user)
+    {
+        $conversation = factory(Conversation::class)->create([
+            Conversation::FIELD_CREATOR_ID => $user->getId(),
+        ]);
+
+        factory(ConversationUser::class)->create([
+            ConversationUser::FIELD_CONVERSATION_ID => $conversation->getId(),
+            ConversationUser::FIELD_USER_ID => $user->getId(),
+        ]);
+
+        return $conversation;
+    }
+
+    private function createReply(Conversation $conversation, User $user, string $text, Carbon $createdAt = null)
+    {
+        $reply = factory(ConversationReply::class)->create([
+            ConversationReply::FIELD_CONVERSATION_ID => $conversation->getId(),
+            ConversationReply::FIELD_SENDER_ID => $user->getId(),
+            ConversationReply::FIELD_TEXT => $text,
+            ConversationReply::CREATED_AT => $createdAt
+        ]);
+
+        factory(ConversationReplyUser::class)->create([
+            ConversationReplyUser::FIELD_USER_ID => $user->getId(),
+            ConversationReplyUser::FIELD_CONVERSATION_REPLY_ID => $reply->getId(),
+        ]);
+
+        return $reply;
     }
 }
